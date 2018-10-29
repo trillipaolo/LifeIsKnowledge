c4408324bd2ec331eff5a79458dc88312d9282e7
Unknown <paolo.trilli@mail.polimi.it>
2018-10-29 19:25:42 +0100

    Added animations and Git plugin

---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour {
+
+    private BoxCollider2D _frontTop;
+    private BoxCollider2D _frontMiddle;
+    private BoxCollider2D _frontDown;
+    private BoxCollider2D _backTop;
+    private BoxCollider2D _backMiddle;
+    private BoxCollider2D _backDown;
+    private BoxCollider2D[] _attackColliders;
+
+    private void Awake() {
+        _attackColliders = GetComponents<BoxCollider2D>();
+
+        _frontTop = _attackColliders[0];
+        _frontMiddle = _attackColliders[1];
+        _frontDown = _attackColliders[2];
+        _backTop = _attackColliders[3];
+        _backMiddle = _attackColliders[4];
+        _backDown = _attackColliders[5];
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            _frontTop.enabled = false;
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+            _frontMiddle.enabled = false;
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+            _frontDown.enabled = false;
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+            _backTop.enabled = false;
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+            _backMiddle.enabled = false;
+        if (Input.GetKeyDown(KeyCode.Alpha6))
+            _backDown.enabled = false;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float WalkSpeed = 5.0f;
+    public float JumpSpeed = 5.0f;
+    public float Gravity = 3.0f;
+    public float YNudge = -2.5f;
+
+    private bool _grounded = false;
+    private float _yVelocity = 0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        UpdateXMovement();
+        UpdateYMovement();
+	}
+
+    void UpdateXMovement() {
+        Vector3 frameMovement = new Vector3(Input.GetAxis("Horizontal") * WalkSpeed,0);
+
+        transform.Translate(frameMovement * Time.deltaTime);
+        if (frameMovement.x != 0) {
+            transform.localScale = new Vector3(Mathf.Sign(frameMovement.x),1,1);
+        }
+    }
+
+    void UpdateYMovement() {
+        if(transform.position.y < YNudge) {
+            _grounded = true;
+            _yVelocity = 0;
+            Vector3 nudge = transform.position;
+            nudge.y = YNudge;
+            transform.position = nudge;
+        }
+
+        if (!_grounded) {
+            _yVelocity -= Gravity * Time.deltaTime;
+        } else {
+            if (Input.GetKeyDown(KeyCode.Space)) {
+                _yVelocity = JumpSpeed;
+                _grounded = false;
+            }
+        }
+        transform.position = new Vector3(transform.position.x,transform.position.y + _yVelocity,0);
+    }
+}
