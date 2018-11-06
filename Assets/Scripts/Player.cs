0dd6aceaed3c47913aab995ea07e55fc623ec301
Unknown <paolo.trilli@mail.polimi.it>
2018-11-06 23:41:24 +0100

    Final Commit

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -28,12 +28,6 @@ public class Player : MonoBehaviour {
         _trRightFoot = rightFoot.transform;
     }
 
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
 	void Update () {
         CheckGrounded();
 
