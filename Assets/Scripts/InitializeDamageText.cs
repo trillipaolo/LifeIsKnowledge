b974a4cd376adf42166214f6492788f8c5438a93
Unknown <paolo.trilli@mail.polimi.it>
2018-11-25 19:42:55 +0100

    Basic attacks done

---
+++ Assets/Scripts/InitializeDamageText.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitializeDamageText : MonoBehaviour {
+
+    private void Awake() {
+        FloatingTextController.Initialize();
+    }
+}
