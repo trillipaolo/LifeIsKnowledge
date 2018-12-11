47d77337b3561c92e3a6a79b6d816db2e5587ecf
Unknown <paolo.trilli@mail.polimi.it>
2018-12-12 00:25:23 +0100

    First sketch

---
+++ Assets/Scripts/BoxBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxBehaviour : EnemyBehaviour {
+
+    public override void TakeDamage(Collider2D collider,float baseDamage,bool unique) {
+        float multiplier = GetMultiplier(collider);
+        float damage = ComputeDamage(baseDamage,multiplier,unique);
+        Color color = ComputeColor(multiplier,unique);
+
+        Vector3 textPosition = (Vector3)collider.offset + transform.position;
+        FloatingTextController.CreateFloatingText(damage.ToString(),textPosition,color);
+    }
+}
