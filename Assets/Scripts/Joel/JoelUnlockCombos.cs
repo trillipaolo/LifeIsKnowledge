82939da35234b705c679143a73a05751db95acb9
Unknown <paolo.trilli@mail.polimi.it>
2018-12-12 14:02:45 +0100

    Added combo unlock

---
+++ Assets/Scripts/Joel/JoelUnlockCombos.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoelUnlockCombos : MonoBehaviour {
+
+	public JoelEnemiesKilled joelEnemiesKilled;
+    private bool _changed = false;
+
+    void Update() {
+        if (_changed) {
+            UpdateUnlockedCombos();
+        }
+    }
+
+    private void UpdateUnlockedCombos() {
+        EnemyNode[] enemiesKilled = joelEnemiesKilled.enemies;
+        for (int i = 0; i < enemiesKilled.Length; i++) {
+            if(enemiesKilled[i].timesKilled >= enemiesKilled[i].timesUnlockCombo) {
+                Combo combo = enemiesKilled[i].comboUnlocked;
+                if (!combo.unlocked) {
+                    combo.unlocked = true;
+                }
+            }
+        }
+    }
+
+    public void KilledEnemy(EnumEnemies enemy) {
+        for(int i = 0; i < joelEnemiesKilled.enemies.Length; i++) {
+            if(joelEnemiesKilled.enemies[i].enemy == enemy) {
+                joelEnemiesKilled.enemies[i].timesKilled++;
+
+                _changed = true;
+                return;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/ScriptableObjects/JoelEnemiesKilled.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ScriptableObjects/JoelEnemiesKilled")]
+public class JoelEnemiesKilled : ScriptableObject {
+
+    public EnemyNode[] enemies;
+
+    public void Reset() {
+        if(enemies != null) {
+            for(int i = 0; i < enemies.Length; i++) {
+                enemies[i].timesKilled = 0;
+            }
+        }
+    }
+
+    public void Initialize() {
+        enemies = new EnemyNode[EnumerationEnemies.Size()];
+        EnumEnemies[] enemiesEnum = EnumerationEnemies.GetEnumArray();
+
+        for(int i = 0; i < enemies.Length; i++) {
+            enemies[i].enemy = enemiesEnum[i];
+            enemies[i].timesKilled = 0;
+        }
+    }
+
+}
