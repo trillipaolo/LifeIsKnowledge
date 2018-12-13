abee2582308dcc1fd74c4b1e1067760f002159cf
Unknown <paolo.trilli@mail.polimi.it>
2018-12-13 12:01:28 +0100

    Added confetti, parameters need to be adjusted

---
+++ Assets/Scripts/SecretAreaScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecretAreaScript : MonoBehaviour {
+
+    private ParticleSystem[] _particles;
+    private Transform _joel;
+
+    private void Awake() {
+        _particles = GetComponentsInChildren<ParticleSystem>();
+        _joel = GameObject.FindWithTag("Player").transform;
+        DisableParticles();
+    }
+
+    private void DisableParticles() {
+        for(int i = 0; i < _particles.Length; i++) {
+            _particles[i].enableEmission = false;
+        }
+    }
+
+    private void EnableParticles() {
+        for (int i = 0; i < _particles.Length; i++) {
+            _particles[i].time = 0;
+            _particles[i].enableEmission = true;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.transform == _joel) {
+            EnableParticles();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        if (other.transform == _joel) {
+            DisableParticles();
+        }
+    }
+}
