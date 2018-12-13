b9119cd1039bdd82099d5ad3a62e0d2bab61ab3c
Unknown <paolo.trilli@mail.polimi.it>
2018-12-13 20:43:22 +0100

    pre prefab

--- Assets/Scripts/Dialogue System/DialogueManager.cs
+++ Assets/Scripts/Dialogue System/DialogueManager.cs
@@ -24,7 +24,7 @@ public class DialogueManager : MonoBehaviour {
         if (Instance == null)
         {
             Instance = this;
-            DontDestroyOnLoad(gameObject);
+            //DontDestroyOnLoad(gameObject);
         }
         else
         {
