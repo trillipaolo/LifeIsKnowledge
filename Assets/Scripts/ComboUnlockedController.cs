142bfb8e478543ec94ecc73fd4258d5ca5a74d8d
Unknown <paolo.trilli@mail.polimi.it>
2018-12-13 18:13:28 +0100

    General fixes

--- Assets/Scripts/ComboUnlockedController.cs
+++ Assets/Scripts/ComboUnlockedController.cs
@@ -5,16 +5,24 @@ using UnityEngine;
 public class ComboUnlockedController : MonoBehaviour {
 
 	public JoelEnemiesKilled joelEnemiesKilled;
+    public JoelCombos joelCombos;
+    public Combo horizontalSpin;
     public bool resetAtStart = false;
 
     private void Awake() {
         if (resetAtStart) {
             joelEnemiesKilled.Reset();
             for (int i = 0; i < joelEnemiesKilled.enemies.Length; i++) {
-                if (joelEnemiesKilled.enemies[i].comboUnlocked.comboName != "HorizontalSpin") {
+                if (joelEnemiesKilled.enemies[i].comboUnlocked.comboName != "Horizontal Spin") {
                     joelEnemiesKilled.enemies[i].comboUnlocked.unlocked = false;
                 }
             }
+
+            joelCombos.combos = new Combo[0];
+
+            horizontalSpin.coloumnSaved = -1;
+            horizontalSpin.rowSaved = -1;
+            horizontalSpin.rotatedSaved = false;
         }
     }
 }
