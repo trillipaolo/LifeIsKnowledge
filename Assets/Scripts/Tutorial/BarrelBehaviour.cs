32817245cd1bb86f77ae3ce9a11b1da53a4a33df
Unknown <paolo.trilli@mail.polimi.it>
2018-12-27 23:20:31 +0100

    Added barrels and buddy

---
+++ Assets/Scripts/Tutorial/BarrelBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrelBehaviour : EnemyBehaviour {
+
+    private bool _broken = false;
+    private Animator _animator;
+
+    private void Awake() {
+        _animator = GetComponent<Animator>();
+    }
+
+    public override void TakeDamage(Collider2D collider,float baseDamage,bool unique, int comboNum, int attackNum) {
+        if (!_broken) {
+            float multiplier = GetMultiplier(collider);
+            float damage = ComputeDamage(baseDamage,multiplier,unique);
+            Color color = ComputeColor(multiplier,unique);
+
+            Vector3 textPosition = (Vector3)collider.offset + transform.position;
+            FloatingTextController.CreateFloatingText(damage.ToString(),textPosition,color);
+
+            health -= damage;
+
+            if (health < 0) {
+                Die();
+            }
+        }
+    }
+
+    public override void Die() {
+        _broken = true;
+
+        for (int i = 0; i < colliders.Length; i++) {
+            colliders[i].enabled = false;
+        }
+
+        _animator.SetTrigger("Broken");
+    }
+}
---
+++ Assets/Scripts/Tutorial/BuddyBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuddyBehaviour : EnemyBehaviour {
+
+    private bool _broken = false;
+    private Animator _animator;
+
+    private void Awake() {
+        _animator = GetComponent<Animator>();
+    }
+
+    public override void TakeDamage(Collider2D collider,float baseDamage,bool unique, int comboNum, int attackNum) {
+        if (!_broken) {
+            float multiplier = GetMultiplier(collider);
+            float damage = ComputeDamage(baseDamage,multiplier,unique);
+            Color color = ComputeColor(multiplier,unique);
+
+            Vector3 textPosition = (Vector3)collider.offset + transform.position;
+            FloatingTextController.CreateFloatingText(damage.ToString(),textPosition,color);
+
+            _animator.SetTrigger("Hit");
+        }
+    }
+}
