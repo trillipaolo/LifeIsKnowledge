146a0261a4c68cb977005e87ce8410f780ca130f
Unknown <erick.venneri@mail.polimi.it>
2019-01-03 22:49:11 +0100

    Power Up ready

---
+++ Assets/Scripts/ScriptableObjects/Health.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ScriptableObjects/Health")]
+public class Health : ScriptableObject {
+
+    public int health;
+    public int initialHealth;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+}
