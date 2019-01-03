37adba878ffce9a68bc445170671d9ef7dba1f58
Alexander <brainzeater@gmail.com>
2019-01-03 15:57:30 +0300

    [WIP] Guard update

--- Assets/Scripts/Physics/GuardMovement.cs
+++ Assets/Scripts/Physics/GuardMovement.cs
@@ -2,15 +2,76 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GuardMovement : EnemyMovementPhysics {
+public class GuardMovement : EnemyMovementPhysics
+{
+    public float chillTimer = 2f;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
+    private float _countdown;
+
+    // Use this for initialization
+    void Start()
+    {
+        base.Start();
+        _countdown = chillTimer;
+    }
+
+    // Update is called once per frame
 	void Update () {
-		
+	    if (!movementDisabled)
+	    {
+	        if (!foundTarget)
+	        {
+	            CheckIfFoundTarget();
+	            Patrol();
+	        }
+	        else
+	        {
+	            FollowAndAttack();
+	        }
+
+	        ApplyGravity();
+	        controller.Move(velocity * Time.deltaTime);
+	        if (controller.collisions.above || controller.collisions.below)
+	        {
+	            velocity.y = 0;
+	        }
+
+	        if (velocity.x > 0)
+	        {
+	            _animator.SetBool("isMoving", true);
+	        }
+
+	        if (controller.collisions.left || controller.collisions.right)
+	        {
+	            Jump();
+	            _animator.SetBool("isMoving", false);
+	        }
+
+//        BlindStrategy();
+	    }
 	}
+
+    public override void Patrol()
+    {
+        float patrolDistance = transform.position.x - _anchor;
+//        Debug.Log(patrolDistance);
+        if (Mathf.Abs(patrolDistance) < patrolRadius)
+        {
+            velocity.x = (facingRight ? 1 : -1) * moveSpeed;
+//			Debug.Log(Mathf.Abs(patrolDistance) + " " + patrolRadius);
+        }
+        else if (Mathf.Abs(patrolDistance) > patrolRadius && (_countdown > 0))
+        {
+            velocity.x = 0;
+            _animator.SetBool("isMoving", false);
+            _countdown -= Time.deltaTime;
+        }
+        else
+        {
+            _countdown = chillTimer;
+            ChangeDirection();
+            _animator.SetBool("isMoving", true);
+            velocity.x = (facingRight ? 1 : -1) * moveSpeed;
+        }
+    }
 }
