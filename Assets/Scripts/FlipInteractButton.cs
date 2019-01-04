109ecc54a7e73fd56ea6e5deeb968615ad1e9e44
Unknown <paolo.trilli@mail.polimi.it>
2019-01-04 18:23:40 +0100

    Added interactable button shown on joel

---
+++ Assets/Scripts/FlipInteractButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipInteractButton : MonoBehaviour {
+
+    private Transform _joel;
+
+    private void Awake() {
+        _joel = transform.root;
+    }
+
+    private void Update() {
+        if(_joel.localScale.x < 0) {
+            transform.localScale = new Vector3(-1,1,1);
+        } else {
+            transform.localScale = new Vector3(1,1,1);
+        }
+    }
+}
---
+++ Assets/Scripts/ShowInteractButton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowInteractButton : MonoBehaviour {
+
+    private GameObject _button;
+
+	private void Start () {
+        _button = GameObject.FindGameObjectsWithTag("Player")[0].transform.root.Find("InteractButton").gameObject;
+	}
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Player") {
+            ShowButton();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        if (other.tag == "Player") {
+            HideButton();
+        }
+    }
+
+    public void ShowButton() {
+        _button.SetActive(true);
+    }
+
+    public void HideButton() {
+        _button.SetActive(false);
+    }
+}
