b6e6fdbcd6c15c6149d3aaef8dd0a826fef28956
Unknown <paolo.trilli@mail.polimi.it>
2019-01-06 18:24:56 +0100

    Added soundtrack

--- Assets/Scripts/CutSceneManager.cs
+++ Assets/Scripts/CutSceneManager.cs
@@ -3,9 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class QuitGame : MonoBehaviour {
+public class CutSceneManager : MonoBehaviour {
+
+    private AudioManager _audioManager;
+
+    private void Start() {
+        _audioManager = AudioManager.instance;
+
+        _audioManager.Pause("Theme");
+        _audioManager.Play("CutScene");
+    }
 
     private void Update() {
+
+        // Quit game and reload scene
         if (Input.GetKeyDown(KeyCode.T)) {
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         } else if (Input.GetKeyDown("escape")) {
