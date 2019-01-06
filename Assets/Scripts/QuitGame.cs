2f66f942a9784589b285867ca326c67262971085
Unknown <paolo.trilli@mail.polimi.it>
2019-01-06 17:58:00 +0100

    Part finished

---
+++ Assets/Scripts/QuitGame.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class QuitGame : MonoBehaviour {
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.T)) {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        } else if (Input.GetKeyDown("escape")) {
+            Application.Quit();
+        }
+    }
+}
