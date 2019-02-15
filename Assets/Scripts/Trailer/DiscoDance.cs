3040f978264bd844d9bf4ae8d2f1ec01a2803a6b
Unknown <paolo.trilli@mail.polimi.it>
2019-02-15 18:55:25 +0100

    VIP inside ;)

---
+++ Assets/Scripts/Trailer/DiscoDance.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiscoDance : MonoBehaviour {
+
+    private SpriteRenderer _sprite;
+
+    public float startTime;
+    public float flipTime;
+    private float _startTime;
+    
+    private void Awake() {
+        _sprite = GetComponent<SpriteRenderer>();
+        _startTime = Time.timeSinceLevelLoad + startTime;
+    }
+    
+    void Update () {
+        float _time = Time.timeSinceLevelLoad;
+
+        if(_time - _startTime > flipTime) {
+            _sprite.flipX = !_sprite.flipX;
+            _startTime = Time.timeSinceLevelLoad;
+        }
+    }
+}
